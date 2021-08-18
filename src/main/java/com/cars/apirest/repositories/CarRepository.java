3e3e5df2a226be60da64b7330659585c400d07bc
Nilson Junior <nilsonjrsilva1997@gmail.com>
2021-08-18 16:02:45 -0300

    feature update and delete

--- src/main/java/com/cars/apirest/repositories/CarRepository.java
+++ src/main/java/com/cars/apirest/repositories/CarRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cars.apirest.models.Car;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
-
+    Car findById(long id);
 }
--- src/main/java/com/cars/apirest/resources/CarResource.java
+++ src/main/java/com/cars/apirest/resources/CarResource.java
@@ -3,14 +3,19 @@ package com.cars.apirest.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.cars.apirest.models.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import com.cars.apirest.repositories.CarRepository;
 import com.cars.apirest.validators.CarValidator;
 
@@ -24,20 +29,71 @@ public class CarResource {
         return carRepository.findAll();
     }
     
+    @GetMapping("/veiculos/{id}")
+    public Car show(@PathVariable(value = "id") long id) {
+        return carRepository.findById(id);
+    }
+    
     @PostMapping("/veiculos")
     public ResponseEntity<Object> create(@RequestBody Car car) {
     	
-    	CarValidator validator = new CarValidator(car);
-    	
-    	List<String> errors = validator.validate();
-    	
-    	if(!errors.isEmpty()) {
+    	try {    	
+    		CarValidator validator = new CarValidator(car);
+    		
+    		List<String> errors = validator.validate();
+    		
+    		if(!errors.isEmpty()) {
+    			ResponseEntity<Object> carResponseEntity = new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    			return carResponseEntity;
+    		} else {    		
+    			car = carRepository.save(car);
+    			ResponseEntity<Object> carResponseEntity = new ResponseEntity<>(car, HttpStatus.ACCEPTED);
+    			return carResponseEntity;
+    		}    	
+    	} catch(Exception ex) {
+    		List<String> errors = new ArrayList<String>();
+
+    		errors.add("Erro ao salvar dados");
     		ResponseEntity<Object> carResponseEntity = new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    		return carResponseEntity;
-    	} else {    		
-    		car = carRepository.save(car);
-    		ResponseEntity<Object> carResponseEntity = new ResponseEntity<>(car, HttpStatus.ACCEPTED);
-    		return carResponseEntity;
-    	}    	
+			return carResponseEntity;
+    	}
+    	
+    }
+    
+    @PutMapping("/veiculos/{id}")
+    public ResponseEntity update(@PathVariable("id") long id, @RequestBody Car car) {
+        try {
+        	CarValidator validator = new CarValidator(car);
+    		
+    		List<String> errors = validator.validate();
+    		
+    		if(!errors.isEmpty()) {
+    			ResponseEntity<Object> carResponseEntity = new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    			return carResponseEntity;
+    		} else {    		
+    			 Car carRecord = carRepository.findById((long) id);
+    	         carRecord.setBrand(car.getBrand());
+    	         carRecord.setDescription(car.getDescription());
+    	         carRecord.setSold(car.getSold());
+    	         carRecord.setVehicle(car.getVehicle());
+    	         carRecord.setYear(car.getYear());
+    	            
+    	         carRepository.save(carRecord);
+
+    	         return new ResponseEntity(carRepository.save(carRecord), HttpStatus.OK);
+    		}      	      
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    @DeleteMapping("/veiculos/{id}")
+    public ResponseEntity delete(@PathVariable("id") long id) {
+        try {
+        	carRepository.deleteById(id);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 }
